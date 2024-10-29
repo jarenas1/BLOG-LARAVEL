d4033b05157306a73702535fdc8b0eacf3f4e57d
Juan José Arenas G. <juanjoarenas1218@gmail.com>
2024-10-29 10:41:03 -0500

    eloquent

--- routes/web.php
+++ routes/web.php
@@ -6,7 +6,7 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\HomeController;
 //OTRO CONTROLADOR
 use App\Http\Controllers\PostController;
-
+use App\Models\Post;
 
 //reicibe una url y devuelve el controlador se llama el controlador y luego el metodo deseado
 Route::get('/controller', [HomeController::class, "index"]);
@@ -32,3 +32,43 @@ Route::get("/laravel/{variable}",function ($variable){
 //     }
 //     return "post {$variable1} COOON 2 parametros";
 // });
+
+Route::get("/prueba", function(){
+    // $post = new Post();
+    // //PASAR DATOS DE PRUEBA GUARDAR
+    // $post->title = "titulo de pruebita2";
+    // $post->content = "Contenido de prueba2";
+    // $post->categoria = "categoria de prueba2";
+
+    // //guardamos la categoria
+    // $post->save();
+
+    // //ACTUALIZAR
+    // $post = Post::find(1); //SI DESEAMOS BUSCAR POR OTRA COSA PONEMOS Post::where(COLUMNNAME, "ValorABuscar)->first() FIRST NOS TRAE EL PRIMER REGISTRO QUE ENCUENTRE 
+    // if($post){
+    //     $post->title = "Titulo de pruebita modificado";
+    //     $post->save();
+    // }
+    // return $post;
+
+    //LEER VARIOS:
+    // $post = Post::all();
+    // return $post;
+    //----------------
+   
+    
+    //ORDENAR REGISTROS Y OBTENER
+    // $post = Post::orderBy("id", "DESC")->get();
+    // return $post;
+
+
+    //TRAER REGISTROS CON COLUMNAS DESEADAS
+    // $post = Post::select("title", "content", "id")->take(2)->get();
+    // return $post;
+
+    //ELIMINAR UN REGISTRO
+    // $post = Post::find(1);
+    // if($post){
+    //     $post->delete();
+    // }
+});
